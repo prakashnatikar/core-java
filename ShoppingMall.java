dbaeb9261712ca24947b304942d97c29461a050d
prakashnatikar <prakashnatikar.xworkz@gmail.com>
2023-08-22 12:33:05 +0530

    example related to veriable data typs

---
+++ ShoppingMall.java
@@ -0,0 +1,16 @@
+class ShoppingMall{
+    public static void main(String mall[]){
+	
+	//datatype variablename  =value/Data 
+	String name="Gopalan";
+	String address="vijayapur";
+	int squarefeet =7000;
+	String numOfShops ="50 shops";
+	boolean isParkingAvailiable=true;
+	System.out.println("the of the mall is"+name);
+	System.out.println("and it is located in"+address);
+	System.out.println("and the area its covers"+squarefeet);
+	System.out.println("and is has "+numOfShops);
+	System.out.println("parking is Availiable"+isParkingAvailiable);
+	}
+}
