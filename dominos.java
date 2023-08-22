87240418d07af16b20f66d6c11bec68ae6268991
prakashnatikar <prakashnatikar.xworkz@gmail.com>
2023-08-22 13:45:47 +0530

    methed overloading

---
+++ dominos.java
@@ -0,0 +1,225 @@
+class dominos{
+public static double search(String Name){
+double price=0.00;
+
+if(Name=="burger king"){
+price=150;
+return price;
+}
+if(Name=="momu"){
+price=50;
+return price;
+}
+if(Name=="melange"){
+price=100;
+return price;
+}
+if(Name=="KFC"){
+price=500;
+return price;
+}
+if(Name=="burger elit"){
+price=70;
+return price;
+}
+if(Name=="easybites"){
+price=150;
+return price;
+}
+if(Name=="boss burger"){
+price=200;
+return price;
+}
+if(Name=="shankrus kichan"){
+price=100;
+return price;
+}
+if (Name=="matan biryani"){
+price =250;
+return price;
+}
+if(Name=="burger king"){
+price=150;
+return price;
+}
+if(Name=="momu"){
+price=50;
+return price;
+}
+if(Name=="melange"){
+price=100;
+return price;
+}
+if(Name=="KFC"){
+price=500;
+return price;
+}
+if(Name=="burger elit"){
+price=70;
+return price;
+}
+if(Name=="easybites"){
+price=150;
+return price;
+}
+if(Name=="boss burger"){
+price=200;
+return price;
+}
+if(Name=="shankrus kichan"){
+price=100;
+return price;
+}
+if (Name=="prid rise"){
+price=50;
+return price;
+}
+if (Name=="maggi"){
+price=35;
+return price;
+}
+if (Name=="masala dose"){
+price=70;
+return price;
+}
+if(Name=="halwa"){
+price=50;
+return price;
+}
+if(Name=="plan dose"){
+price=70;
+return price;
+}
+if(Name=="matan masala"){
+price=300;
+return price;
+}
+if(Name=="gulam jamun"){
+price=20;
+return price;
+}
+if(Name=="masala rise"){
+price=50;
+return price;
+}
+if(Name=="chicken masala"){
+price=200;
+return price;
+}
+if(Name=="setdose"){
+price=60;
+return price;
+}
+if(Name=="chicken thanduri"){
+price=250;
+return price;
+}
+if(Name=="jeera rise"){
+price=50;
+return price;
+}
+if(Name=="chicken lolipop"){
+price=500;
+return price;
+}
+if(Name=="matan lolipop"){
+price=500;
+return price;	
+}
+if(Name=="panipuri"){
+price=50;
+return price;
+}
+if(Name=="jilebi"){
+price=40;
+return price;
+}
+if(Name=="balusha"){
+price=50;
+return price;
+}
+if(Name=="banjabi thali"){
+price=300;
+return price;
+}
+if(Name=="chicken role"){
+price=500;
+return price;
+}
+if(Name=="gobi manchori"){
+price=100;
+return price;
+}
+if(Name=="palaka panir"){
+price=100;
+return price;
+}
+if(Name=="egg rise"){
+price=50;
+return price;
+}
+if(Name=="palaka panir"){
+price=100;
+return price;
+}
+if(Name=="egg rise"){
+price=50;
+return price;
+}
+if(Name=="chicken lolipop"){
+price=500;
+return price;
+}
+if(Name=="matan lolipop"){
+price=500;
+return price;	
+}
+if(Name=="panipuri"){
+price=50;
+return price;
+}
+if(Name=="jilebi"){
+price=40;
+return price;
+}
+if(Name=="balusha"){
+price=50;
+return price;
+}
+if(Name=="banjabi thali"){
+price=300;
+return price;
+}
+if (Name=="maggi"){
+price=35;
+return price;
+}
+if (Name=="masala dose"){
+price=70;
+return price;
+}
+if(Name=="halwa"){
+price=50;
+return price;
+}
+if(Name=="plan dose"){
+price=70;
+return price;
+}
+if(Name=="matan masala"){
+price=300;
+return price;
+}
+if(Name=="gulam jamun"){
+price=20;
+return price;
+}
+if(Name=="chai chai baat"){
+price=100;
+return price; 
+}
+return price;
+
+}	
+
+}
