2fa6d93c06d09e1fcb8eac5b4172bfcc3cd2a3ae
prakashnatikar <prakashnatikar.xworkz@gmail.com>
2023-08-22 10:49:52 +0530

    commiting example

---
+++ maggis.java
@@ -0,0 +1,15 @@
+class maggis {
+
+   public static void main(String nodles[]){
+      int price=10;
+	  String Mgfmaggi="03-04-2023";
+	  String expdate ="05-07-2024";
+	  String quantity="30gm";
+	  
+	        System.out.println("the maggi prise of "+price);
+			System.out.println("the manufactionring maggi of"+Mgfmaggi);
+			System.out.println("expire date of manufactionring maaggi is"+expdate);
+			System.out.println("the maggi quatinty is"+quantity);
+			}
+	}
+			
